172958dd87e8e43063c0f83c58031ec3a970c804
Donald Ness <donald.ness@gmail.com>
2013-08-15 15:19:12 -0600

    Add some more default programs. Fix problem with Bluetooth block with ID 0

--- programs.js
+++ programs.js
@@ -50,6 +50,53 @@ module.exports = {
 		'void act()',
 		'{',
 		'}'
+	].join('\n'),
+
+	"Ramp.c": [
+		'void setup()',
+		'{',
+		'	block_value = 0;',
+		'}',
+		'',
+		'void loop()',
+		'{',
+		'	think();',
+		'	act();',
+		'}',
+		'',
+		'void think()',
+		'{',
+		'	wait(50);',
+		'	block_value = (block_value == 255) ? 0 : block_value + 1;',
+		'}',
+		'',
+		'void act()',
+		'{',
+		'	set_bar_graph(block_value);',
+		'}'
+	].join('\n'),
+
+	"Pong.c": [
+		'void setup()',
+		'{',
+		'}',
+		'',
+		'void loop()',
+		'{',
+		'	act();',
+		'}',
+		'',
+		'void act()',
+		'{',
+		'	static int i = 0;',
+		'	static int d = 1;',
+		'	clear_bar(i);',
+		'	if (i == 9) d = -1;',
+		'	if (i == 0) d = 1;',
+		'	i += d;',
+		'	set_bar(i);',
+		'	wait(500);',
+		'}'
 	].join('\n')
 
 }
