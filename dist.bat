c77bf38827a9db8875ccbe150f8441bd86072832
Eric Lundby <Eric.Lundby@modrobotics.com>
2014-10-28 16:30:15 -0600

    Replace additional icon in nw.exe

--- dist.bat
+++ dist.bat
@@ -4,6 +4,7 @@ set RESHACK="C:\Program Files (x86)\Resource Hacker\ResHacker.exe"
 
 echo "Building installer..."
 call %RESHACK% -addoverwrite "%dp0%build\nw.exe","%dp0%build\nw.exe","%dp0%resources\nw.ico",ICONGROUP,IDR_MAINFRAME,1033
+call %RESHACK% -addoverwrite "%dp0%build\nw.exe","%dp0%build\nw.exe","%dp0%resources\nw.ico",ICON,1,1033
 call rmdir /s /q dist 2> NUL
 call mkdir dist 2> NUL
 call candle.exe -nologo "resources\msi\dist.wxs" -out "dist\tmp.wixobj" -ext WixUIExtension
